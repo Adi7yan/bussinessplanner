af02b7976de70b25828f834fb8a6452d23423867
Adithyan <adithyan2525@gmail.com>
2021-08-04 11:35:43 +0530

    commiting code

---
+++ src/main/java/com/example/bussinessplanner/BussinessplannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bussinessplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussinessplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussinessplannerApplication.class, args);
+		System.out.println("inside bussinessplanner");
+	}
+}
---
+++ src/test/java/com/example/bussinessplanner/BussinessplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bussinessplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussinessplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
